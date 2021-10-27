cc92b28c51b763a3cf884db66f9aa38c26afa1d8
Luis <luis@hypnoticsiege.codes>
2021-10-27 14:27:16 -0400

    Reeeevert

--- index.js
+++ index.js
@@ -13,31 +13,6 @@ module.exports = function format(seconds) {
         var mnts = Math.floor(seconds / 60);
         seconds -= mnts * 60;
         const timeFormat = days + " Days, " + hrs + " Hours, " + mnts + " Minutes, and " + seconds + " Seconds"
+
     return timeFormat
-};
-
-
-/**
- * Day
- * @returns Current Week Day Name
- */
-
-module.exports = function day() {
-    const dayNumber = new Date().getDay()
-    const dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
-    const day = dayNames[dayNumber]
-    return day
-}
-
-
-/**
- * Month
- * @returns Current Month Name
- */
-
-module.exports = function month() {
-    const monthNumber = new Date().getMonth
-    const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-    const month = monthNames[monthNumber]
-    return month
-}
+}; 
